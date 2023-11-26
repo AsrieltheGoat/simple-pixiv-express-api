9b6214dbb6b1859bb31b84b89d7e75b8738ba8d5
“Asriel <asriel@dreemurrs.eu.org>
2023-11-26 17:49:38 +0700

    Refactor image retrieval and error handling

--- index.js
+++ index.js
@@ -11,69 +11,82 @@ app.get("/", (req, res) => {
     const word = req.query.word || "初音ミク"; // "Hatsune Miku" in kanji as a default
 
     pixiv.refreshAccessToken(process.env.REFRESH_TOKEN).then(() => {
-        return pixiv.searchIllust(word).then(async (json) => {
-            const illust =
-                json.illusts[Math.floor(Math.random() * json.illusts.length)];
-            const ori_url = illust.image_urls.large;
-            const url = ori_url.replace(
-                "https://i.pximg.net/",
-                "https://i.pixiv.cat/"
-            );
+        return pixiv
+            .searchIllust(word)
+            .then(async (json) => {
+                const illust =
+                    json.illusts[
+                        Math.floor(Math.random() * json.illusts.length)
+                    ];
 
-            const artist_ori_url = illust.user.profile_image_urls.medium;
-            const artist_url = artist_ori_url.replace(
-                "https://i.pximg.net/",
-                "https://i.pixiv.cat/"
-            );
+                // console.log(illust);
+                if (illust == undefined || illust == null) {
+                    return res.json({ error: "No result found." });
+                }
 
-            // Convert image to base64
-            const artwork_base64 = await imageToBase64(url);
-            const artist_base64 = await imageToBase64(artist_url);
+                const ori_url = illust.image_urls.large;
+                const url = ori_url.replace(
+                    "https://i.pximg.net/",
+                    "https://i.pixiv.cat/"
+                );
 
-            // console.log(illust);
-            const outJson = {
-                info: [
-                    {
-                        artwork: [
-                            {
-                                id: illust.id,
-                                title: illust.title,
-                                created_time: illust.create_date,
-                                page_count: illust.page_count,
-                                illust_r18: illust.x_restrict,
-                                illust_url:
-                                    "https://www.pixiv.net/en/artworks/" +
-                                    illust.id,
-                                image_url: url,
-                                tags: illust.tags.map((tag) => tag.name),
-                            },
-                        ],
+                const artist_ori_url = illust.user.profile_image_urls.medium;
+                const artist_url = artist_ori_url.replace(
+                    "https://i.pximg.net/",
+                    "https://i.pixiv.cat/"
+                );
 
-                        artist: [
-                            {
-                                artist: illust.user.name,
-                                artist_preferred_name: illust.user.account,
-                                artist_url:
-                                    "https://www.pixiv.net/member.php?id=" +
-                                    illust.user.id,
-                                artist_id: illust.user.id,
-                                artist_image_url: artist_url,
-                            },
-                        ],
-                    },
-                ],
+                // Convert image to base64
+                const artwork_base64 = await imageToBase64(url);
+                const artist_base64 = await imageToBase64(artist_url);
 
-                image: [
-                    {
-                        artwork: artwork_base64,
-                        artist: artist_base64,
-                    },
-                ],
-            };
+                const outJson = {
+                    info: [
+                        {
+                            artwork: [
+                                {
+                                    id: illust.id,
+                                    title: illust.title,
+                                    created_time: illust.create_date,
+                                    page_count: illust.page_count,
+                                    illust_r18: illust.x_restrict,
+                                    illust_url:
+                                        "https://www.pixiv.net/en/artworks/" +
+                                        illust.id,
+                                    image_url: url,
+                                    tags: illust.tags.map((tag) => tag.name),
+                                },
+                            ],
 
-            // console.log(outJson);
-            res.send(outJson);
-        });
+                            artist: [
+                                {
+                                    artist: illust.user.name,
+                                    artist_preferred_name: illust.user.account,
+                                    artist_url:
+                                        "https://www.pixiv.net/member.php?id=" +
+                                        illust.user.id,
+                                    artist_id: illust.user.id,
+                                    artist_image_url: artist_url,
+                                },
+                            ],
+                        },
+                    ],
+
+                    image: [
+                        {
+                            artwork: artwork_base64,
+                            artist: artist_base64,
+                        },
+                    ],
+                };
+
+                // console.log(outJson);
+                res.send(outJson);
+            })
+            .catch((err) => {
+                // console.log(err);
+                res.json({ error: "Internal server error!" });
+            });
     });
 });
 
